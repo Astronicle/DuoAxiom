f3a550d65bbece9b3c55b5463210d93cb1c1668e
Abdul Rehman <astronicle78@gmail.com>
2025-09-02 09:46:40 +0530

    Initialized React + Springboot project

---
+++ backend/src/main/java/com/astronicle/duoaxiom/DuoaxiomApplication.java
@@ -0,0 +1,13 @@
+package com.astronicle.duoaxiom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoaxiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoaxiomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/astronicle/duoaxiom/DuoaxiomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astronicle.duoaxiom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoaxiomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
